585e3e80aea84bc1f89da1dffc0022e4b2cdba4e
mielecarmine <mielecarmine993@gmail.com>
2024-04-12 15:56:04 +0200

    fix: api

--- routes/api.php
+++ routes/api.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::resource('api/projects', ProjectController::class)->only(['index', 'show', 'store', 'update', 'destroy']);
+Route::apiResource('projects', ProjectController::class)->except(['edit','create']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
